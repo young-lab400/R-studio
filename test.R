54dd9a969370e0e136327aeacfd7d3fb36b8ce75
young-lab400 <samps3114@gmail.com>
2017-08-03 17:28:35 +0800

    del hahaha
    新增註解
    BL test尚未完成
    ks.test 改成pnorm與常態分布 做檢定
    目標 :
    function() 計算<0.05的個數
    隨機產生常態數 與資料做2 sample ks.test

--- test.R
+++ test.R
@@ -1,7 +1,7 @@
 library("cluster")
-library("clusterSim")
 library("MASS")
 library("modeest")
+library("clusterSim")
 library("nlme")
 library("analysis")
 library(stats)
@@ -20,10 +20,10 @@ for (x in 1:length(filesA))
   CD_read <- CD_read[,-3]
   CD_read <- CD_read[,-2]
   CD_read <- CD_read[,-1]
-  CD_read
+  #CD_read
   CD_read_numeric <- as.numeric(unlist(CD_read))
   CD_normalized[[x]] <- data.Normalization(CD_read_numeric,type="n1")
-  CD_normalized[[x]]
+  #CD_normalized[[x]]
 }
 
 
@@ -41,7 +41,7 @@ for(x in 1:length(filesB))
   DD_read <- DD_read[,-1]
   DD_read_numeric <- as.numeric(unlist(DD_read))
   DD_normalized[[x]] <- data.Normalization(DD_read_numeric,type = "n1")
-  DD_normalized[[x]]
+  #DD_normalized[[x]]
 }
 
 FolderC = "~/data/Disease data/feature"
@@ -51,48 +51,67 @@ f_normalized = NULL
 for (x in 1:length(filesC))
 {
   temppath = paste(FolderC,filesC[x],sep="/")
-  temppath
+  #temppath
   f_read <-read.table(temppath, header=T,sep="\t",row.names=1,stringsAsFactors=FALSE,fileEncoding = "UCS-2LE")
   f_read[f_read=="."]<-NA
   f_read <- as.data.frame(t(f_read), stringsAsFactors=FALSE)
-  f_read
+  #f_read
   #刪除方法去除缺失
   ppp <- complete.cases(f_read)
   ppp
+  #月份正規化
   f_read <- f_read[complete.cases(f_read),]
-  f_read
+  f_read[1,] <-f_read[1,]/31*30
+  f_read[3,] <-f_read[3,]/31*30
+  f_read[5,] <-f_read[5,]/31*30
+  f_read[7,] <-f_read[7,]/31*30
+  f_read[8,] <-f_read[8,]/31*30
+  f_read[10,] <-f_read[10,]/31*30
+  f_read[12,] <-f_read[12,]/31*30
   
   f_read_numeric <- as.numeric(unlist(f_read))
-  f_read_numeric
+  #f_read_numeric
   f_normalized[[x]] <- data.Normalization(f_read_numeric,type="n1")
-  f_normalized[[x]]
+  #f_normalized[[x]]
 }
 
-#CD
-CD_result <- data.frame()
-for (x in 1:length(filesC))
+#BL_test for CD 
+for(x in 1:length(filesA))
 {
-  for (y in 1:length(filesA))
-  {
-    f_normalized[[x]]
-    CD_normalized[[y]]
-    CD_result[x,y]<-ks.test(unique(f_normalized[[x]]),unique(CD_normalized[[y]]),alternative = "two.sided")$p.value
-  }
+  CD_BL_result = Box.test(CD_normalized[[x]],lag = 12,type = "Ljung-Box")
+  CD_BL_result
+  CD_BL_result_2 = data.frame(t(matrix(unlist(CD_BL_result))))
+  names(CD_BL_result_2) <- names(CD_BL_result)
+}
+CD_BL_result
+CD_BL_result[3]
+CD_BL_result_2
+
+#ks.test for CD
+CD_result <- NULL
+for (y in 1:length(filesA))
+{
+ #CD_result[x,y]<-ks.test(unique(f_normalized[[x]]),unique(CD_normalized[[y]]),alternative = "two.sided")$p.value
+  CD_result[y]<-ks.test(unique(CD_normalized[[y]]),"pnorm")$p.value
 }
 names(CD_result) <- filesA
-row.names(CD_result) <- filesC
-CD_result
-#DD
-DD_result <- data.frame()
-for (x in 1:length(filesC))
+
+#ks.test for DD
+DD_result <- NULL
+for (y in 1:length(filesB))
 {
-  for (y in 1:length(filesB))
-  {
-    f_normalized[[x]]
-    DD_normalized[[y]]
-    DD_result[x,y]<-ks.test(unique(f_normalized[[x]]),unique(DD_normalized[[y]]),alternative = "two.sided")$p.value
-  }
+    #DD_result[x,y]<-ks.test(unique(f_normalized[[x]]),unique(DD_normalized[[y]]),alternative = "two.sided")$p.value
+    DD_result[y]<-ks.test(unique(DD_normalized[[y]]),"pnorm")$p.value
 }
 names(DD_result) <- filesB
-row.names(DD_result) <- filesC
+
+#ks.test for feature
+f_result <- NULL
+for (x in 1:length(filesC))
+{
+  f_result[x]<-ks.test(unique(f_normalized[[x]]),"pnorm")$p.value
+}
+names(f_result) <- filesC
+CD_result
 DD_result
+f_result
